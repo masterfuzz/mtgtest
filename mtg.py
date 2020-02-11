ba46de0ec665db6b0baf6c02fc539cf67e3974f8
Robert Martens <rmartens@embotics.com>
2020-02-11 16:12:49 -0500

    card stuff and keywords

--- mtg.py
+++ mtg.py
@@ -14,16 +14,20 @@ def rec(model, deck):
     
     return model.similar_by_vector(center)
 
+KEYWORDS = set()
+
 class Card:
     def __init__(self, j):
         self.name = j['name']
-        self.text = j.get('text')
+        self.text = j.get('text', "")
         self.colors = j.get('colors', [])
         self.types = j.get('types', [])
         self.subtypes = j.get('subtypes', [])
         self.manaCost = j.get('manaCost')
         self.power = j.get('power')
         self.toughness = j.get('toughness')
+        self.printings = j['printings']
+        self.keywords = [k for k in self.text.split() if k in KEYWORDS]
 
     def tokens(self):
         yield from self.colors
@@ -32,11 +36,16 @@ class Card:
         yield "MC_" + str(self.manaCost)
         yield "POWER_" + str(self.power)
         yield "TOUGH_" + str(self.toughness)
-        if self.text:
-            yield from self.text.replace(self.name, "SELF").split()
+        # if self.text:
+        #     yield from self.text.replace(self.name, "SELF").split()
+        yield from self.keywords
 
 
 def get_cards():
+    with open("cards/Keywords.json") as fh:
+        keyword_json = json.load(fh)
+        KEYWORDS.update(keyword_json['abilityWords'] + keyword_json['keywordAbilities'] + keyword_json['keywordActions'])
+
     with open("cards/AllCards.json") as fh:
         card_json = json.load(fh)
     return {name: Card(cj) for name, cj in card_json.items()}
